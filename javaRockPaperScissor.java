5c4de0032d65b36a19b4bcb8a9a582386014a101
Brad <Brad.nguyen2512@gmail.com>
2022-09-24 22:58:07 -0400

    9/24 - Update 4: javaRockPaperScissor| Deliverable

--- javaRockPaperScissor.java
+++ javaRockPaperScissor.java
@@ -1,28 +1,44 @@
-//imports
 import java.util.Scanner;
 import java.lang.Math.*;
 
-
 public class javaRockPaperScissor
 {
-    //user
-    static String user()
+
+    public static void main(String[] args)
     {
-        //userInform
-        System.out.println("Rock");
-        System.out.println("Paper");
-        System.out.println("Scissor");
+        //introduction
+        System.out.println("Welcome to javaRockPaperScissor. \nWould you like to play against me? \nThere are 3 moves you can make: Rock, Paper, Scissor. \nThere rules are: \n   Rock beats Scissor, \n   Paper beats Rock, \n   Scissor beats Paper");
         
-        //input
+        //computerRNG
+        int computerRNG = (int) Math.round(Math.random()*2);
+
+        //computerMove
+        String computerMove = "";
+
+        //computer Logic
+        if (computerRNG == 0)
+        {
+            computerMove = "Rock";
+        } else if (computerRNG == 1)
+        {
+            computerMove = "Paper";
+        } else if (computerRNG == 2)
+        {
+            computerMove = "Scissor";
+        }
+
+        //scanner
         Scanner input = new Scanner(System.in);
+
+        //player
         System.out.print("Which move do you want to play? ");
-        String userInput = input.next();
+        String userMove = input.next();
 
         //validMove
         boolean validMove = true;
 
         //validMove Logic
-        if (userInput.toUpperCase().equals("ROCK") || userInput.toUpperCase().equals("PAPER") || userInput.toUpperCase().equals("SCISSOR"))
+        if (userMove.toLowerCase().equals("rock") || userMove.toLowerCase().equals("paper") || userMove.toLowerCase().equals("scissor"))
         {
             validMove = true;
         } else
@@ -31,10 +47,9 @@ public class javaRockPaperScissor
         }
         while (validMove == false)
         {
-            System.out.print("Invalid Move! Try a different move: ");
-            userInput = input.next();
-
-            if (userInput.toUpperCase().equals("ROCK") || userInput.toUpperCase().equals("PAPER") || userInput.toUpperCase().equals("SCISSOR"))
+            System.out.print("Invalid Move! Try a different one: ");
+            userMove = input.next();
+            if (userMove.toLowerCase().equals("rock") || userMove.toLowerCase().equals("paper") || userMove.toLowerCase().equals("scissor"))
             {
                 validMove = true;
             } else
@@ -43,57 +58,23 @@ public class javaRockPaperScissor
             }
         }
 
-        //shoot
-        System.out.println("SHOOT!");
+        //gamePlay
+        System.out.println("The computer has played " + computerMove);
 
-        //outcome
-        return userInput;
-    }
-
-    //computer
-    static Double computer()
-    {
-        double randomNumber = Math.random() * 2;
-        double computerRNG = Math.round(randomNumber);
-        
-        //outcome
-        return computerRNG;
-    }
-    public static void main(String[] args)
-    {
-        //user Function
-        user();
-        
-        //computer Function
-        computer();
-
-        //computerMove
-        String computerMove = "";
-        
-        //computerMove Logic
-        if (computer() == 0)
+        //gamePlay Logic
+        if (userMove.toLowerCase().equals(computerMove.toLowerCase()))
         {
-            computerMove = "Rock";
-        } else if (computer() == 1)
+            System.out.println("It is a tie!!");
+        } else if (userMove.toLowerCase().equals("rock") && computerMove.toLowerCase().equals("scissor")
+        || userMove.toLowerCase().equals("paper") && computerMove.toLowerCase().equals("rock")
+        || userMove.toLowerCase().equals("scissor") && computerMove.toLowerCase().equals("paper"))
         {
-            computerMove = "Paper";
-        } else if (computer() == 2)
+            System.out.println("You won! Congratulations!!");
+        } else if (userMove.toLowerCase().equals("rock") && computerMove.toLowerCase().equals("paper")
+        || userMove.toLowerCase().equals("paper") && computerMove.toLowerCase().equals("scissor")
+        || userMove.toLowerCase().equals("scissor") && computerMove.toLowerCase().equals("rock"))
         {
-            computerMove = "Scissor";
-        }
-
-        //game Logic
-        if (user().equals(computerMove))
-        {
-            System.out.println("It's a tie!");
-        } else if (user().toLowerCase().equals("rock") & computerMove.toLowerCase().equals("scissor")
-            || user().toLowerCase().equals("paper") & computerMove.toLowerCase().equals("rock")
-            || user().toLowerCase().equals("scissor") & computerMove.toLowerCase().equals("paper"))
-        {
-            System.out.println("You won! Congratulations");
-        } else
-        {
-            System.out.println("You lost! Bad Luck");
+            System.out.println("You lost! Tough luck!!");
         }
     }
 }
